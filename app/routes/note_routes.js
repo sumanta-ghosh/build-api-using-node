864511d322f0b0ac63b6f73766f5d61ec75027a7
Sumanta Ghosh <sumanta.ghosh@infoway.us>
2017-10-30 19:16:26 +0530

    Add get routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,17 @@
 module.exports = function (app, db) {
+    var ObjectID = require('mongodb').ObjectID;
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
     app.post('/notes', (req, res) => {
         const note = {text: req.body.body, title: req.body.title};
         db.collection("notes").insert({text: 'Sumanta kr ghosh', title: 'Sumanta'});
