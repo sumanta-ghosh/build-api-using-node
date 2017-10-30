ab27c92e83c7cd46c07ec46eeb352e56477326d8
Sumanta Ghosh <sumanta.ghosh@infoway.us>
2017-10-30 19:17:32 +0530

    Add delete route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -23,4 +23,16 @@ module.exports = function (app, db) {
             }
         });
     });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
 };
